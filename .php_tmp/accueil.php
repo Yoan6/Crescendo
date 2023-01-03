abb770cfb3d081ad901e493613d5f93c3ee3f066
LAW Julien <julien.law@etu.univ-grenoble-alpes.fr>
2023-01-03 11:56:47 +0100

    Fix page produit

---
+++ .php_tmp/accueil.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Crescendo</title>
+        <link rel="stylesheet" type="text/css" href="../design/header.css">
+		<link rel="stylesheet" type="text/css" href="../design/crescendo.css">
+	</head>
+	<body class="dark-mode">
+        <?php include(__DIR__.'/header.php'); ?>
+		
+		<main id="accueil">
+            <div id="banner">
+                <img  src="../design/image/accueil/coeur_concert.jpeg">
+            </div>
+            <nav>
+                <ul class="conteneur">
+                    <li>
+                        <a>
+                            <img src="../design/image/accueil/vetement.jpg">
+                            <h2>Vêtements</h2>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <img src="../design/image/accueil/vetement.jpg">
+                            <h2>Instruments</h2>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <img src="../design/image/accueil/vetement.jpg">
+                            <h2>Autres</h2>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+
+            </br>
+
+		</main>
+		
+		
+		
+        <?php include(__DIR__.'/footer.php'); ?>	
+	</body>
+	<script src="../js/crescendo.js"></script>
+    <script src="../js/header.js"></script>
+
+</html>
