9f41a44cd35e9c40f4d037f69b345cd31e0c9b52
LAW Julien <julien.law@etu.univ-grenoble-alpes.fr>
2023-01-05 15:40:10 +0100

    Changement par rapport à la maquette

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  php: "/usr/bin/php",              //Linux 
-  //php: "C:\\xampp\\php\\php.exe", // windows 
+  //php: "/usr/bin/php",              //Linux 
+  php: "C:\\xampp\\php\\php.exe", // windows 
     injectBody: true, // activer les updates en temps réel
   }
