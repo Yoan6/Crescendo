5489b02c6df9c06c5b8b60a66ec64cfc38120157
Cyprien Boscher <cyprien.boscher@etu.univ-grenoble-alpes.fr>
2023-01-05 15:55:13 +0100

    amélioration header

--- view/header.php
+++ view/header.php
@@ -1,6 +1,6 @@
 <header>
     <div id="firstStage"> 
-        <a class="logo" href="accueil.php"><img src="../design/image/crescendo_logo_black.png"  alt="logo"></a>
+        <a class="logo" href="accueil.php"><img src="../design/image/crescendo_logo_black.svg"  alt="logo"></a>
         <form action="rechercher.php" method="GET" class="conteneur">
             <input class="searchBar" type="search" list="searchList" placeholder="Rechercher...">
             <datalist id="searchList">
@@ -8,15 +8,15 @@
                     <option value="Elvis Presley"></option>
                     <option value="The quick brown fox jumps over a lazy dog."></option>
             </datalist>
-            <a id="loupe" href="https://www.google.com/"><img src="../design/image/search.png" alt="loupe"></a>
+            <a id="loupe" href="https://www.google.com/"><img src="../design/image/search.svg" alt="loupe"></a>
         </form>
                     
-        
+  <a id="creationArticle">Vends tes articles</a>
         
 
 <div class="container">
   <button class="btn" id="btn">
-  <img src="../design/image/user.png"  alt="profile">
+  <img src="../design/image/user.svg"  alt="profile">
     <i class="bx bx-chevron-down" id="arrow"></i>
   </button>
 
