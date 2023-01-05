ca96f78a404f71364f1b428b700b6408c96bad98
delannyo <yoan.delannoy@etu.univ-grenoble-alpes.fr>
2023-01-05 18:37:51 +0100

    Modifications login et inscription

---
+++ view/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <meta name="author" content="Yoan Delannoy" />
+    <link rel="stylesheet" type="text/css" href="../design/connexion.css">
+    <link rel="stylesheet" type="text/css" href="../design/header.css">
+		<link rel="stylesheet" type="text/css" href="../design/crescendo.css">
+  </head>
+  <body class="dark-mode">
+    
+    <main id="connexion">
+      <h1>Connexion</h1>
+      <form action="../controler/login.ctrl.php" method="post">
+        <p>
+          <label for="login">Adresse email / Pseudo : </label>
+          <br>
+          <input class="input" id="login" type="text" name="login" value="" placeholder="ex : Artus" required>
+        </p>
+        <p>
+            <label for="password">Mot de passe (au moins 12 caractères) : </label>
+            <br>
+            <input class="input" id="password" type="password" name="password" value="" placeholder="ex : ahdfukqshxsb" required>
+        </p>
+        <a class="MDPForgotten" href="">Mot de passe oublié ?</a>
+        <button class="connexion" type="submit" name="connection">SE CONNECTER</button>
+      </form>
+      
+        
+        <form action="../controler/inscription.ctrl.php" method="post">
+            <button class="inscription" type="submit" name="inscription">S'INSCRIRE</button>
+        </form>
+    </main>
+  </body>
+</html>
