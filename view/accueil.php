ea78caa527256f9d3f78683d7fc8f35d78996391
Cyprien Boscher <cyprien.boscher@etu.univ-grenoble-alpes.fr>
2023-01-06 02:00:57 +0100

    accueil terminé

--- view/accueil.php
+++ view/accueil.php
@@ -6,6 +6,8 @@
         <link rel="stylesheet" type="text/css" href="../design/header.css">
 		<link rel="stylesheet" type="text/css" href="../design/crescendo.css">
         <link rel="stylesheet" type="text/css" href="../design/accueil.css">
+        <link rel="stylesheet" type="text/css" href="../design/article.css">
+
 	</head>
 	<body class="dark-mode">
         <?php include(__DIR__.'/header.php'); ?>
@@ -45,10 +47,38 @@
                 
             </div>
             
-            <div id="articleSemaine">
+            <div class="conteneur" id="articleSemaine">
                 <div>
                     <h1>Les articles de la semaine</h1>
+                    
+                    <div class="article">
+                    
+                        <div id="heart">
+                            <img src="../design/image/heart.svg">
+                        </div>
+                        <div id="responsive">
+                            <div id="photo">
+                                <img src="../design/image/articles/veste-elton-john.png">
+                            </div>
+                            <div id="center">
+                                <div>
+                                    <h3>Veste d'Elton John</h3>
+                                    <h3>10 000 $</h3>
+                                </div>
+                                <section>
+                                    <p>Concert à New York d’Elton John, le 22 février 2022. Veste queue de pie à imprimé floral, sur la blablablabaalblabalbalablalbalbalbbalbalabablalblbaablabablablblbablalabblabalalbababll </p>
+                                </section>
+                                <a href="">Voir l'enchère</a>
+                            </div>
+                        </div>
+                        
+                        <div id="likes">
+                            <img src="../design/image/icon/thumb up white.png">
+                            <p>654</p>
+                            <img src="../design/image/icon/thumb down white.png">
+                        </div>
 
+                    </div>
 
                 </div>
             </div>
