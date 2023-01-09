953c24bcd1e745d76f3001d72958a5a79316c73b
Cyprien Boscher <cyprien.boscher@etu.univ-grenoble-alpes.fr>
2023-01-10 00:52:42 +0100

    amélioration footer

--- view/footer.php
+++ view/footer.php
@@ -2,32 +2,94 @@
     <nav>
         <ul class="conteneur">
             <li class="conteneur">
-                <h4>A propos</h4>
-                <ul class="tiret">
-                    <li><a href="https://www.google.com/">Mentions légales</a></li>
-                    <li><a href="https://www.google.com/">Affiliation</a></li>
-                    <li><a href="https://www.google.com/">Conditions générales</a></li>
-                </ul>
-            </li>
-            <li class="conteneur">
-                <h4>Aide</h4>
-                <ul class="tiret">
-                    <li><a href="https://www.google.com/">Comment vendre ?</a></li>
-                    <li><a href="https://www.google.com/">Comment acheter ?</a></li>
-                    <li><a href="https://www.google.com/">FAQ</a></li>
-                    <li><a href="https://www.google.com/">Espace sécurité</a></li>
-                </ul>
-            </li>
-            <li class="conteneur">
-                <h4>Communauté</h4>	
-                <ul id="logosCommunaute">
-                    <li><a href="https://www.google.com/"><svg class="iconTaille" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_iconCarrier"> <rect x="0" fill="none" width="20" height="20"></rect> <g> <path d="M2.89 2h14.23c.49 0 .88.39.88.88v14.24c0 .48-.39.88-.88.88h-4.08v-6.2h2.08l.31-2.41h-2.39V7.85c0-.7.2-1.18 1.2-1.18h1.28V4.51c-.22-.03-.98-.09-1.86-.09-1.85 0-3.11 1.12-3.11 3.19v1.78H8.46v2.41h2.09V18H2.89c-.49 0-.89-.4-.89-.88V2.88c0-.49.4-.88.89-.88z"></path> </g> </g></svg></a></li>
-                    <li><a href="https://www.google.com/"><svg class="iconTaille" fill="#ffffff" viewBox="-1.5 0 19 19" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_iconCarrier"><path d="M15.917 1.666v15.833H.083V1.666zM13.743 6.02a4.702 4.702 0 0 1-1.353.37 2.365 2.365 0 0 0 1.036-1.303 4.725 4.725 0 0 1-1.496.572 2.359 2.359 0 0 0-4.017 2.149 6.685 6.685 0 0 1-4.856-2.462 2.357 2.357 0 0 0 .728 3.146 2.339 2.339 0 0 1-1.067-.295v.03a2.359 2.359 0 0 0 1.89 2.311 2.362 2.362 0 0 1-1.064.04 2.36 2.36 0 0 0 2.202 1.637 4.733 4.733 0 0 1-2.928 1.01 4.838 4.838 0 0 1-.562-.034 6.702 6.702 0 0 0 10.318-5.647c0-.102-.002-.203-.007-.304a4.785 4.785 0 0 0 1.176-1.22z"></path></g></svg></a></li>
-                    <li><a href="https://www.google.com/"><svg class="iconTaille" viewBox="0 0 48 48" version="1.1" id="Shopicons" xmlns="http://www.w3.org/2000/svg" x="0" y="0" xml:space="preserve" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_iconCarrier"> <style>.st1{fill:#fff}.st2,.st3{fill:none;stroke:#666;stroke-width:4;stroke-miterlimit:10}.st3{stroke-width:6}</style> <g id="instagram_00000127030415805702900630000013396224003006281109_"> <path d="M0-.019h48v48H0v-48z" fill="none"></path> <path d="M36.019 8A3.986 3.986 0 0 1 40 11.981v24.038A3.986 3.986 0 0 1 36.019 40H11.981A3.986 3.986 0 0 1 8 36.019V11.981A3.986 3.986 0 0 1 11.981 8h24.038m0-4H11.981A7.98 7.98 0 0 0 4 11.981v24.038A7.98 7.98 0 0 0 11.981 44h24.038A7.98 7.98 0 0 0 44 36.019V11.981A7.98 7.98 0 0 0 36.019 4z"></path> <path d="M24 34c-5.514 0-10-4.486-10-10s4.486-10 10-10 10 4.486 10 10-4.486 10-10 10zm0-16c-3.309 0-6 2.691-6 6s2.691 6 6 6 6-2.691 6-6-2.691-6-6-6z"></path> <circle cx="35" cy="13" r="2"></circle> </g> </g></svg></a></li>
-                </ul>	
-                <div class='contact'>
-                    Nous contacter
+                <div>
+                    <h4>A propos</h4>
+                    <ul class="tiret">
+                        <li><a href="https://www.google.com/">Mentions légales</a></li>
+                        <li><a href="https://www.google.com/">Affiliation</a></li>
+                        <li><a href="https://www.google.com/">Conditions générales</a></li>
+                    </ul>
                 </div>
+
+            </li>
+            <li class="conteneur">
+                <div>
+                    <h4>Aide</h4>
+
+                    <ul class="tiret">
+                        <li><a href="https://www.google.com/">Comment vendre ?</a></li>
+                        <li><a href="https://www.google.com/">Comment acheter ?</a></li>
+                        <li><a href="https://www.google.com/">FAQ</a></li>
+                        <li><a href="https://www.google.com/">Espace sécurité</a></li>
+                    </ul>
+                </div>
+
+            </li>
+            <li class="conteneur">
+                <div>
+                    <h4>Communauté</h4>
+                    <ul id="logosCommunaute">
+                        <li><a href="https://www.google.com/"><svg class="iconTaille" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <rect x="0" fill="none" width="20" height="20"></rect>
+                                        <g>
+                                            <path
+                                                d="M2.89 2h14.23c.49 0 .88.39.88.88v14.24c0 .48-.39.88-.88.88h-4.08v-6.2h2.08l.31-2.41h-2.39V7.85c0-.7.2-1.18 1.2-1.18h1.28V4.51c-.22-.03-.98-.09-1.86-.09-1.85 0-3.11 1.12-3.11 3.19v1.78H8.46v2.41h2.09V18H2.89c-.49 0-.89-.4-.89-.88V2.88c0-.49.4-.88.89-.88z">
+                                            </path>
+                                        </g>
+                                    </g>
+                                </svg></a></li>
+                        <li><a href="https://www.google.com/"><svg class="iconTaille" fill="#ffffff"
+                                    viewBox="-1.5 0 19 19" xmlns="http://www.w3.org/2000/svg" class="cf-icon-svg">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <path
+                                            d="M15.917 1.666v15.833H.083V1.666zM13.743 6.02a4.702 4.702 0 0 1-1.353.37 2.365 2.365 0 0 0 1.036-1.303 4.725 4.725 0 0 1-1.496.572 2.359 2.359 0 0 0-4.017 2.149 6.685 6.685 0 0 1-4.856-2.462 2.357 2.357 0 0 0 .728 3.146 2.339 2.339 0 0 1-1.067-.295v.03a2.359 2.359 0 0 0 1.89 2.311 2.362 2.362 0 0 1-1.064.04 2.36 2.36 0 0 0 2.202 1.637 4.733 4.733 0 0 1-2.928 1.01 4.838 4.838 0 0 1-.562-.034 6.702 6.702 0 0 0 10.318-5.647c0-.102-.002-.203-.007-.304a4.785 4.785 0 0 0 1.176-1.22z">
+                                        </path>
+                                    </g>
+                                </svg></a></li>
+                        <li><a href="https://www.google.com/"><svg class="iconTaille" viewBox="0 0 48 48" version="1.1"
+                                    id="Shopicons" xmlns="http://www.w3.org/2000/svg" x="0" y="0" xml:space="preserve"
+                                    fill="#ffffff">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                    <g id="SVGRepo_iconCarrier">
+                                        <style>
+                                            .st1 {
+                                                fill: #fff
+                                            }
+
+                                            .st2,
+                                            .st3 {
+                                                fill: none;
+                                                stroke: #666;
+                                                stroke-width: 4;
+                                                stroke-miterlimit: 10
+                                            }
+
+                                            .st3 {
+                                                stroke-width: 6
+                                            }
+                                        </style>
+                                        <g id="instagram_00000127030415805702900630000013396224003006281109_">
+                                            <path d="M0-.019h48v48H0v-48z" fill="none"></path>
+                                            <path
+                                                d="M36.019 8A3.986 3.986 0 0 1 40 11.981v24.038A3.986 3.986 0 0 1 36.019 40H11.981A3.986 3.986 0 0 1 8 36.019V11.981A3.986 3.986 0 0 1 11.981 8h24.038m0-4H11.981A7.98 7.98 0 0 0 4 11.981v24.038A7.98 7.98 0 0 0 11.981 44h24.038A7.98 7.98 0 0 0 44 36.019V11.981A7.98 7.98 0 0 0 36.019 4z">
+                                            </path>
+                                            <path
+                                                d="M24 34c-5.514 0-10-4.486-10-10s4.486-10 10-10 10 4.486 10 10-4.486 10-10 10zm0-16c-3.309 0-6 2.691-6 6s2.691 6 6 6 6-2.691 6-6-2.691-6-6-6z">
+                                            </path>
+                                            <circle cx="35" cy="13" r="2"></circle>
+                                        </g>
+                                    </g>
+                                </svg></a></li>
+                    </ul>
+                    <a href="mailto: crescendo@gmail.com" class='contact'>
+                        Nous contacter
+                    </a>
+                </div>
+
             </li>
         </ul>
     </nav>
