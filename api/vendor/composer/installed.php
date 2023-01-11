e4aef4e678a5d0159e2762cd101b7ab390468475
LAW Julien <lawj@hostname.iut2.upmf-grenoble.fr>
2023-01-11 16:55:58 +0100

    ==========ajout de l'api PAYPAL=============

---
+++ api/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8d194e76bdd33b91d00015964e6c78f0b231e61d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8d194e76bdd33b91d00015964e6c78f0b231e61d',
+    ),
+    'paypal/paypal-checkout-sdk' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '19992ce7051ff9e47e643f28abb8cc1b3e5f1812',
+    ),
+    'paypal/paypalhttp' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7b09c89c80828e842c79230e7f156b61fbb68d25',
+    ),
+  ),
+);
