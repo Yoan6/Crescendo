e095ba75fd3c51f5acd39842181905f6b7e4d23d
montanip <pablo.montanier@etu.univ-grenoble-alpes.fr>
2023-01-16 15:23:10 +0100

    mise en forme cmtAcheter + cmtVendre

--- view/commentAcheter.php
+++ view/commentAcheter.php
@@ -11,36 +11,39 @@
 
 <body class="dark-mode">
     <?php include(__DIR__."/header.php"); ?>
-        <main id="cmtAcheter">
+        <main class="cmt">
             <div>
                 <h1>Comment acheter</h1>
                 <p>
-                    Bienvenue sur Crescendo, vous êtes à la recherche d’un article porté par l’un(e) 
+                    Bienvenue sur Crescendo, vous êtes à la recherche d'un article porté par l'un(e) 
                     de vos artistes favoris ? Vous avez toqué à la bonne porte. Crescendo vous propose 
-                    un service fiable et simple d’utilisation..
+                    un service fiable et simple d'utilisation..
                 </p>
+                <br>
                 <p>
-                    Une fois arrivée sur notre page d’accueil, vous avez la possibilité de naviguer à travers 
-                    notre site et de mener autant de recherches que vous le voulez. Une fois qu’un article vous 
-                    a tapé dans l’œil et que vous souhaitez soumettre une enchère, vous devez impérativement vous 
+                    Une fois arrivée sur notre page d'accueil, vous avez la possibilité de naviguer à travers 
+                    notre site et de mener autant de recherches que vous le voulez. Une fois qu'un article vous 
+                    a tapé dans l'œil et que vous souhaitez soumettre une enchère, vous devez impérativement vous 
                     connecter. Pour cela, cliquez sur le bouton « Se Connecter » qui se trouve en haut à droite 
-                    de la page d’accueil. Deux options s’offrent alors à vous, la possibilité de se connecter si 
-                    vous disposez déjà d’un compte ou la possibilité de s’inscrire. Une fois connecté, vous aurez 
+                    de la page d'accueil. Deux options s'offrent alors à vous, la possibilité de se connecter si 
+                    vous disposez déjà d'un compte ou la possibilité de s'inscrire. Une fois connecté, vous aurez 
                     alors la possibilité de soumettre une enchère pour les articles qui vous ont plu, noter des 
                     articles ou encore proposer vos propres articles à la vente.
                 </p>
+                <br>
                 <p>
-                    Admettons que vous soyez fan d’Elton John et que l’un des articles qu’il a porté vous a particulièrement 
-                    plu. Cette veste par exemple est mise en vente par un autre utilisateur et vous souhaitez l’acquérir. 
-                    Cliquez sur l’article et vous aurez alors accès à davantage d’informations concernant l’article : son 
-                    prix de départ, le montant actuel qu’atteint l’enchère, le nom du vendeur, une description de l’article 
-                    ainsi que d'avantages d’images.
+                    Admettons que vous soyez fan d'Elton John et que l'un des articles qu'il a porté vous a particulièrement 
+                    plu. Cette veste par exemple est mise en vente par un autre utilisateur et vous souhaitez l'acquérir. 
+                    Cliquez sur l'article et vous aurez alors accès à davantage d'informations concernant l'article : son 
+                    prix de départ, le montant actuel qu'atteint l'enchère, le nom du vendeur, une description de l'article 
+                    ainsi que d'avantages d'images.
                 </p>
+                <br>
                 <p>
                     En indiquant un montant dans la rubrique « Votre nouveau prix » vous pourrez alors soumettre une 
                     nouvelle enchère, puis cliquez sur le bouton « Valider » afin de renseigner vos coordonnées PayPal, 
                     ce qui nous permettra de nous assurer que vous disposez bien de la somme que vous avez renseigné, 
-                    sans pour autant que cette dernière ne vous soit prélevée tant que vous n’avez pas remporté l’enchère.
+                    sans pour autant que cette dernière ne vous soit prélevée tant que vous n'avez pas remporté l'enchère.
                 </p>
             </div>
         </main>
