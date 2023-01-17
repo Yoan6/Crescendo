2e941f1bf15d067aaf8d6bfc9a0f9762334921e3
LAW Julien <lawj@hostname.iut2.upmf-grenoble.fr>
2023-01-17 09:33:45 +0100

    petite correction sur la création d'une enchère

--- model/Article.class.php
+++ model/Article.class.php
@@ -482,7 +482,6 @@ class Article
         $data = array();
         Article::generationDynamiqueData($data, $choixEtvaleurs,$choixObligatoiresEtvaleurs);
         
-        var_dump($data, $query);
         $dao = DAO::get();
         $tableContenantLeNombre = $dao->query($query, $data);
         return $tableContenantLeNombre[0][0];
