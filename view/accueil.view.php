caa44d1bdb9ad5c6a408aa7ae662abb56a41241c
LAW Julien <lawj@hostname.iut2.upmf-grenoble.fr>
2023-01-19 17:31:56 +0100

    mauvais lien

--- view/accueil.view.php
+++ view/accueil.view.php
@@ -19,7 +19,7 @@
             <img src="../design/image/accueil/concert.jpg">
             <div id="banner-text">
                 <h3> Au plus proche de vos artistes préférés ! </h3>
-                <a id="explore" href="../controller/rechercheChoix.ctrl.php?orderBy[]=AlaUne"> Explore dès maintenant !</a>
+                <a id="explore" href="../controller/rechercheALaUne.ctrl.php"> Explore dès maintenant !</a>
             </div>
         </div>
 
