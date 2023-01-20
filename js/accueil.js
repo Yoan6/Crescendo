897d691ee66c3cbd715f6e07c611ceb2cd72a2df
Cyprien Boscher <cyprien.boscher@etu.univ-grenoble-alpes.fr>
2023-01-20 06:48:52 +0100

    amélioration design pop up cookie

--- js/accueil.js
+++ js/accueil.js
@@ -2,10 +2,13 @@
 
 var switchConsentement_oblige = document.getElementsByClassName('switchConsentement oblige');
 
-
-
+var obligatoire = document.getElementsByClassName('obligatoire');
+var accepterCookies = document.getElementById('accepterCookies');
 
 for(var i = 0; i < switchConsentement_oblige.length; i++) {
-switchConsentement_oblige[i].checked = true;
+switchConsentement_oblige[i].click();
 
+switchConsentement_oblige[i].style.pointerEvents = "none";
+switchConsentement_oblige[i].checked = true;
 }
+
